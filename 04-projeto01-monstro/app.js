d0edccb12582344b89300572c62e9cb746a21b40
Edmundo Abreu e Lima <engedmundo@gmail.com>
2022-05-02 13:53:37 -0300

    create life bars

--- 04-projeto01-monstro/app.js
+++ 04-projeto01-monstro/app.js
@@ -1,10 +1,13 @@
 new Vue({
     el: '#app',
     data: {
-        
+        playerLife: 15,
+        monsterLife: 5,
     },
     computed: {
-
+        hasResult() {
+            return this.playerLife === 0 || this.monsterLife === 0
+        }
     },
     method: {
 
